986ec3aa951f8d6620ad38c01cd4995b5ae13b18
Pradhuman121 <155950271+Pradhuman121@users.noreply.github.com>
2024-03-14 12:58:19 +0530

    Update poc.py

--- poc.py
+++ poc.py
@@ -7,4 +7,4 @@ def hello():
     return 'Hello, World! This is my CI/CD demo.'
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+    app.run(debug=True, host='127.0.0.1', port=5001) 
