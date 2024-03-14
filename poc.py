5d3b1d47e761d03077b11c8d08af88bd8a0c4e9d
Pradhuman121 <155950271+Pradhuman121@users.noreply.github.com>
2024-03-14 15:35:04 +0530

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.title("Hello, Streamlit!")
+    st.title("Hello, Streamlit!. Welcome to Jenkins Club")
     st.write("Hello, World!")
 
 if __name__ == "__main__":
